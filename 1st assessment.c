30bce4b8fa9eb0d02ca65e415917bda8615e9dad
harsh635 <78408802+harsh635@users.noreply.github.com>
2021-06-19 14:36:18 +0530

    Add files via upload

---
+++ 1st assessment.c
@@ -0,0 +1,9 @@
+This website aims to provide service to people on the basis of their needs.
+With our project named "UR SERVICE", in this we aim to provide valuable assisstance to people at a very reasonable rate.
+In this assessment, the person will be able to visit our website and see its contents.
+There will be a account option in which the person has to login/register to begin.
+There will be option on which type of service he/she needs.
+Whether he/she needs a maid , a bouncer, a cook, a caretaker, a driver, etc for their household needs.
+for eg. if they need a cook , we will provide them with a cook on hourly basis and charge a reasonable price.
+There will be two sides of work. one-whether you need any service, second-you have registered to earn i.e. to give service. '
+It will help people in earning by providing service to others. It is a unique project and will be very helpful for people living in meteropolitan cities.
